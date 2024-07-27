e4d51d9ec0d988624e5634267dc3de7d633760f0
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-07-27 19:59:07 +0530

    All Function Created

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const symbolsCheck = document.querySelector("#symbols")
 const indicator = document.querySelector("[data-indicator]")
 const generateBtn = document.querySelector(".generateBtn")   
 const allCheckbox = document.querySelectorAll("input[type='checkbox']") 
+const symbols = '!@#$%^&*(){}[]=<>/,.'
 
 let password = ""
 let passwordLength = 10
@@ -35,5 +36,52 @@ function setIndicator(color)
 
 function getRndInteger(min, max) 
 {
-    Math.floor(Math.random() * (max - min + 1) ) + min
+    return Math.floor(Math.random() * (max - min) ) + min
+}
+
+function generateRandomNumber()
+{
+    return getRndInteger(0, 9)
+}
+
+function generateLowerCase()
+{
+    return String.fromCharCode(getRndInteger(97, 123))
+}
+
+function generateUpperCase()
+{
+    return String.fromCharCode(getRndInteger(65, 91))
+}
+
+function generateSymbol()
+{
+    const randNum = getRndInteger(0, symbols.length)
+    return symbols.charAt(randNum)
+}
+
+function calcStrength()
+{
+    let hasUpper = false
+    let hasLower = false
+    let hasNum = false
+    let hasSym = false
+    if(uppercaseCheck.checked) hasUpper = true
+    if(lowercaseCheck.checked) hasLower = true
+    if(numbersCheck.checked) hasNum = true
+    if(symbolsCheck.checked) hasSym = true
+
+    if(hasUpper && hasLower && (hasNum || hasSym) && passwordLength >= 8)
+    {
+        setIndicator("#0f0")
+    }
+    else if((hasUpper || hasLower) && (hasNum || hasSym) && passwordLength >= 6)
+    {
+        setIndicator("#ff0")
+    }
+    else
+    {
+        setIndicator("#f00")
+    }
+
 }
