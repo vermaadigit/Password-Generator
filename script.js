15af088961b820e5db6ad8def99fd80309723f3c
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2024-07-28 17:40:58 +0530

    Password Display Added

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const symbols = '!@#$%^&*(){}[]=<>/,.'
 
 let password = ""
 let passwordLength = 10
-let checkCount = 1
+let checkCount = 0
 handleSlider() 
 //Set Strength Indicator color to Grey
 
@@ -105,6 +105,20 @@ async function copyContent()
     }, 2000)
 }
 
+function shufflePassword(array)
+{
+    //Fisher Yates Algorithm
+    for(let i = array.length - 1; i > 0; i--)
+    {
+        const j = Math.floor(Math.random() * (i + 1))
+        const temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+    }
+    let str = ""
+    array.forEach((el) => (str += el))
+    return str
+}
 function handleCheckBoxChange()
 {
     checkCount = 0
@@ -138,7 +152,7 @@ copyBtn.addEventListener('click', () => {
 generateBtn.addEventListener('click', () => {
     //none of the checkbox is selected
 
-    if(checkCount <= 0) return
+    if(checkCount == 0) return
 
     if(passwordLength < checkCount)
     {
@@ -148,7 +162,48 @@ generateBtn.addEventListener('click', () => {
 
     //Lets start the journey to find new password
 
+    console.log("Generating Password")
     //remove old password
     password = ""   
-    
+
+    //lets put the stuff mentioned by checkboxes
+    // if(uppercaseCheck.checked) password += generateUpperCase()
+    // if(lowercaseCheck.checked) password += generateLowerCase()
+    // if(numbersCheck.checked) password += generateRandomNumber()
+    // if(symbolsCheck.checked) password += generateSymbol()
+
+    let funcArr = []
+
+    if(uppercaseCheck.checked) funcArr.push(generateUpperCase)
+    if(lowercaseCheck.checked) funcArr.push(generateLowerCase)
+    if(numbersCheck.checked) funcArr.push(generateRandomNumber)
+    if(symbolsCheck.checked) funcArr.push(generateSymbol)
+
+    //compulsory stuff
+
+    for(let i = 0; i < funcArr.length; i++)
+    {
+        password += funcArr[i]()
+    }
+    console.log("compulsory stuff done")
+
+    //remaining stuff
+
+    for(let i = 0; i < passwordLength - funcArr.length; i++)
+    {
+        let randIndex = getRndInteger(0, funcArr.length)
+        password += funcArr[randIndex]()
+    } 
+    console.log("remaining stuff done")
+
+    //Shuffle the password
+    password = shufflePassword(Array.from(password))
+    console.log("Shuffling done")
+
+    //show in UI
+    passwordDisplay.value = password
+    console.log("Password Displayed")
+
+    //calulate strength
+    calcStrength()
 })
